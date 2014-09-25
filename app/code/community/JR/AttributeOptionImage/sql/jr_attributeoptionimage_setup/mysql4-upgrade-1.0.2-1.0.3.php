855835edf2aba58571a1d2253c7722e278bdfef6
William Tran <william@aligent.com.au>
2014-09-25 16:08:29 +0930

    Updated setup script

--- app/code/community/JR/AttributeOptionImage/sql/jr_attributeoptionimage_setup/mysql4-upgrade-1.0.2-1.0.3.php
+++ app/code/community/JR/AttributeOptionImage/sql/jr_attributeoptionimage_setup/mysql4-upgrade-1.0.2-1.0.3.php
@@ -4,9 +4,9 @@ $installer = $this;
 /* @var $installer Mage_Core_Model_Resource_Setup */
 
 $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
-$setup->getConnection()->changeColumn(
+
+$setup->getConnection()->addColumn(
     $setup->getTable('catalog/eav_attribute'),
-    'option_image',
     'enable_option_image',
     array(
         'type'  =>  Varien_Db_Ddl_Table::TYPE_SMALLINT,
@@ -15,14 +15,5 @@ $setup->getConnection()->changeColumn(
         'comment'   =>  'Enable Option Image per attribute'
     )
 );
-//$setup->getConnection()->addColumn(
-//    $setup->getTable('catalog/eav_attribute'),
-//    'has_option_image',
-//    array(
-//        'type'  =>  Varien_Db_Ddl_Table::TYPE_BOOLEAN,
-//        'nullable'  => true,
-//        'comment'   =>  'Enable Option Image per attribute'
-//    )
-//);
 
 $installer->endSetup();
