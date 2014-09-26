9fd50bddfc0f04090ae75eee3d7629a1138e5afc
William Tran <william@aligent.com.au>
2014-09-26 09:44:07 +0930

    Committed missing file, check for enable_option_image per attribute

--- app/code/community/JR/AttributeOptionImage/Block/Catalog/Product/Attribute/Edit/Tab/Options.php
+++ app/code/community/JR/AttributeOptionImage/Block/Catalog/Product/Attribute/Edit/Tab/Options.php
@@ -5,7 +5,9 @@ class JR_AttributeOptionImage_Block_Catalog_Product_Attribute_Edit_Tab_Options e
     public function __construct()
     {
         parent::__construct();
-        $this->setTemplate('jr/catalog/product/attribute/options.phtml');
+        if ($this->getAttributeObject()->getEnableOptionImage() == true) {
+            $this->setTemplate('jr/catalog/product/attribute/options.phtml');
+        }
     }
 
     public function getOptionValues()
