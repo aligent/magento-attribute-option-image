b7bd3aa6ceaa33b338acc2dd08bb8ce2bb4c754d
Jonathan Day <jonathan@aligent.com.au>
2014-10-19 10:37:14 +1030

    add support for Solr which only returns option labels instead of option IDs. Projects using Solr will need to use the new getAttributeOptionIdFromLabel helper method before looking up the image/thumb/hex by option ID.

--- app/code/community/JR/AttributeOptionImage/Helper/Data.php
+++ app/code/community/JR/AttributeOptionImage/Helper/Data.php
@@ -2,6 +2,41 @@
 
 class JR_AttributeOptionImage_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    /**
+     * Having Solr in use for Layered Navigation messes with attributes (see below).
+     *
+     * @author Jonathan Day <jonathan@aligent.com.au>
+     * @return bool
+     */
+    public function isSolrActive(){
+        if(class_exists(Mage::getConfig()->getHelperClassName('enterprise_search'),false) && Mage::helper('enterprise_search')->getIsEngineAvailableForNavigation()){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * When Solr is used for Layered Navigation it returns the option Label instead of the Option Id. Given that "Black" could be an option label on multiple attributes,
+     * we have to load the attribute to determine the option_id to therefore load the linked image/thumb/hex. blech.
+     *
+     * @author Jonathan Day <jonathan@aligent.com.au>
+     * @param $vAttributeLabel
+     * @param $vAttributeIdentifier
+     * @return int
+     * @throws Mage_Core_Exception
+     */
+    public function getAttributeOptionIdFromLabel($vAttributeLabel, $vAttributeIdentifier){
+        if(is_string($vAttributeIdentifier)){
+            $oAttribute = Mage::getModel('catalog/resource_eav_attribute')->loadByCode(Mage_Catalog_Model_Product::ENTITY, $vAttributeIdentifier);
+        }
+        else {
+            $oAttribute = Mage::getModel('catalog/resource_eav_attribute')->load($vAttributeIdentifier);
+        }
+        $iResult = $oAttribute->getSource()->getOptionId($vAttributeLabel);
+        return $iResult;
+    }
+
     public function getAttributeOptionImage($optionId)
     {
         $images = $this->getAttributeOptionImages();
